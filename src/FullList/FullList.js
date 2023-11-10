81a79b08da101807a3b3214bad5fb08eb0880cd9
Anudeep K K <anudeep2003@gmail.com>
2023-11-10 10:13:08 +0530

    complete list

--- src/FullList/FullList.js
+++ src/FullList/FullList.js
@@ -1,114 +1,136 @@
-import Navbar from "../Navbar/Navbar";
-import { useState, useEffect } from "react";
-import darkbg4 from "../Signup/darkbg4.jpeg";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+ 
+  import React, { useState, useEffect } from "react";
+  import { getFirestore, collection, getDocs } from "firebase/firestore";
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+  import Navbar from "../Navbar/Navbar";
+  import darkbg4 from "../Signup/darkbg4.jpeg";
+  
+  const FullList = () => {
+    const [user, setUser] = useState(null); // To track user authentication status
+    const [participants, setParticipants] = useState([]);
+    const [search, setSearch] = useState("");
+  
+    const db = getFirestore();
+    const auth = getAuth();
+    const participantsCollection = collection(db, "Participant");
+  
+    useEffect(() => {
+      const signInWithDefaultCredentials = async () => {
+        const email = "anudeep@gmail.com"; // Replace with the actual email
+        const password = "123456"; // Replace with the actual password
+  
+        try {
+          await signInWithEmailAndPassword(auth, email, password);
+          const user = auth.currentUser;
+          setUser(user);
+        } catch (error) {
+          console.error("Error logging in with default credentials:", error);
+        }
+      };
+  
+      signInWithDefaultCredentials();
+  
+      const fetchData = async () => {
+        try {
+          const snapshot = await getDocs(participantsCollection);
+          const participantData = snapshot.docs.map((doc) => ({
+            uid: doc.id,
+            ...doc.data(),
+          }));
+          setParticipants(participantData);
+        } catch (error) {
+          console.error("Error fetching participants:", error);
+        }
+      };
+  
+      fetchData();
+    }, [participantsCollection, auth]);
 
-const FullList = () => {
-  const [participants, setParticipants] = useState([]);
-  const [search, setSearch] = useState("");
-
-  const db = getFirestore();
-  const participantsCollection = collection(db, "Participant");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const snapshot = await getDocs(participantsCollection);
-        const participantData = snapshot.docs.map((doc) => ({
-          uid: doc.id,
-          ...doc.data(),
-        }));
-        setParticipants(participantData);
-      } catch (error) {
-        console.error("Error fetching participants:", error);
-      }
+    const filteredData = participants.filter((row) => {
+      return (
+        row.uid.toLowerCase().includes(search.toLowerCase()) ||
+        row.Name.toLowerCase().includes(search.toLowerCase()) ||
+        row.Contact.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+  
+    const tableStyle = {
+      border: "5px solid orange",
+      borderCollapse: "collapse",
+      width: "100vw",
     };
-
-    fetchData();
-  }, [participantsCollection]);
-
-  const tableStyle = {
-    border: "5px solid orange",
-    borderCollapse: "collapse",
-    width: "100vw",
-  };
-
-  const thStyle = {
-    border: "5px solid orange",
-    padding: "10px",
-    color: "orange",
-  };
-
-  const tdStyle = {
-    border: "5px solid orange",
-    padding: "8px",
-    color:"white"
-  };
-
-  const filteredData = participants.filter((row) => {
+  
+    const thStyle = {
+      border: "5px solid orange",
+      padding: "10px",
+      color: "orange",
+    };
+  
+    const tdStyle = {
+      border: "5px solid orange",
+      padding: "8px",
+      color: "white",
+    };
+  
+    const handleSearchChange = (e) => {
+      setSearch(e.target.value);
+    };
+  
     return (
-      row.uid.toLowerCase().includes(search.toLowerCase()) ||
-      row.Name.toLowerCase().includes(search.toLowerCase()) ||
-      row.Contact.toLowerCase().includes(search.toLowerCase())
-    );
-  });
-
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  return (
-    <>
-      <div
-        style={{
-          background: `url(${darkbg4})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          height: "100vh",
-        }}
-      >
-        <Navbar />
-        <center>
-          <h1 style={{ color: "white", position: "relative", top: "20vh" }}>
-            COMPLETE USER'S LIST
-          </h1>
-        </center>
-        <input
-          type="text"
-          placeholder="Search Participant..."
-          value={search}
-          onChange={handleSearchChange}
+      <>
+        <div
           style={{
-            marginTop: "20vh",
-            width: "100vw",
-            padding: "10px",
-            borderRadius: "24px",
-            backgroundColor: "orange",
+            background: `url(${darkbg4})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            height: "100vh",
           }}
-        />
-        <br></br>
-        <br></br>
-        <table style={tableStyle}>
-          <thead>
-            <tr>
-              <th style={thStyle}>UID</th>
-              <th style={thStyle}>NAME</th>
-              <th style={thStyle}>CONTACT</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredData.map((row) => (
-              <tr key={row.uid}>
-                <td style={tdStyle}>{row.uid}</td>
-                <td style={tdStyle}>{row.Name}</td>
-                <td style={tdStyle}>{row.Contact}</td>
+        >
+          <Navbar />
+          <center>
+            <h1 style={{ color: "white", position: "relative", top: "20vh" }}>
+              COMPLETE USER'S LIST
+            </h1>
+          </center>
+          {user && (
+            <input
+              type="text"
+              placeholder="Search Participant..."
+              value={search}
+              onChange={handleSearchChange}
+              style={{
+                marginTop: "20vh",
+                width: "100vw",
+                padding: "10px",
+                borderRadius: "24px",
+                backgroundColor: "orange",
+              }}
+            />
+          )}
+          <br></br>
+          <br></br>
+          <table style={tableStyle}>
+            <thead>
+              <tr>
+                <th style={thStyle}>UID</th>
+                <th style={thStyle}>NAME</th>
+                <th style={thStyle}>CONTACT</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
-};
-
-export default FullList;
+            </thead>
+            <tbody>
+              {filteredData.map((row) => (
+                <tr key={row.uid}>
+                  <td style={tdStyle}>{row.uid}</td>
+                  <td style={tdStyle}>{row.Name}</td>
+                  <td style={tdStyle}>{row.Contact}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  };
+  
+  export default FullList;
+  
--- src/firebaseAuth/auth.js
+++ src/firebaseAuth/auth.js
@@ -1,13 +1,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAxYsYp2u3gRp3IVhLaBFUZMQFMvAtbTqM",
+//   authDomain: "adminpaneltesting.firebaseapp.com",
+//   projectId: "adminpaneltesting",
+//   storageBucket: "adminpaneltesting.appspot.com",
+//   messagingSenderId: "1054063209428",
+//   appId: "1:1054063209428:web:af89cc87873eaf5f1f4441",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAxYsYp2u3gRp3IVhLaBFUZMQFMvAtbTqM",
-  authDomain: "adminpaneltesting.firebaseapp.com",
-  projectId: "adminpaneltesting",
-  storageBucket: "adminpaneltesting.appspot.com",
-  messagingSenderId: "1054063209428",
-  appId: "1:1054063209428:web:af89cc87873eaf5f1f4441",
+  apiKey: "AIzaSyBN8H-HRd_jVDLprNAt96KC3iQKs1qqgZA",
+  authDomain: "aerophilia-2023.firebaseapp.com",
+  projectId: "aerophilia-2023",
+  storageBucket: "aerophilia-2023.appspot.com",
+  messagingSenderId: "171266886388",
+  appId: "1:171266886388:web:feb4f7debe7eae045f6ca1",
 };
 
 // Initialize Firebase
